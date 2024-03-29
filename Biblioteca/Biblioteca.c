3e87f20aeb29f38238d2907dd9ea11fe7d08c68a
albertojbe <141800257+albertojbe@users.noreply.github.com>
2024-03-29 14:20:57 -0300

    Update Biblioteca.c

--- Biblioteca/Biblioteca.c
+++ Biblioteca/Biblioteca.c
@@ -10,7 +10,7 @@ typedef struct
     int copias;
 }Biblioteca;
 
-void inicializar(Biblioteca *livro, char tituloLivro[100], char autorLivro, int anoLivro, int copiasLivro){
+void inicializar(Biblioteca *livro, char tituloLivro[100], char autorLivro[50], int anoLivro, int copiasLivro){
     strcpy(livro->titulo, tituloLivro);
     strcpy(livro->autor, autorLivro);
     livro->ano = anoLivro;
@@ -22,11 +22,25 @@ int emprestarCopia(Biblioteca *livro){
 }
 int devolverCopia(Biblioteca *livro){
     return ++livro->copias;
-
+}
+void verificarCopias(Biblioteca *livro){
+    if (livro->copias > 0){
+        printf("Ha: %d copias.", livro->copias);
+    }
+    else{
+        printf("Nao ha copias");
+    }
+}
+void imprimirDetalhes(Biblioteca *livro){
+    printf("Nome do livro: %s\n", livro->titulo);
+    printf("O autor do livro e: %s\n", livro->autor);
+    printf("O ano de lancamento do livro e: %d\n", livro->ano);
+    printf("O numero de copias e: %d\n", livro->copias);
 }
 
-
-
-int main{
+int main(){
+    Biblioteca ASOIAF;
+    inicializar(&ASOIAF, "Guerra dos Tronos", "George Martin", 1960, 5);
+    imprimirDetalhes(&ASOIAF);
     return 0;
 }
